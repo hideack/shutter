5195cbd4837433720e15aff52763af08c989d161
Hideaki Terai <hideack@pepabo.com>
2023-05-05 12:36:38 +0900

    例外処理を追加

--- app/controllers/api/v1/shutters_controller.rb
+++ app/controllers/api/v1/shutters_controller.rb
@@ -65,8 +65,13 @@ module Api
       end
 
       def fetch_html(url)
-        user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
-        URI.open(url, "User-Agent" => user_agent).read
+        user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/112.0.0.0 Safari/537.36"
+        begin
+          URI.open(url, "User-Agent" => user_agent).read
+        rescue OpenURI::HTTPError => e
+          Rails.logger.error("Failed to fetch HTML from #{url}: #{e.message}")
+          nil
+        end
       end
 
       def validate_key
